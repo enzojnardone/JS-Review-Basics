42205227c889bd4c390a73e40f9359715b515250
enzojnardone <101941591+enzojnardone@users.noreply.github.com>
2022-05-09 16:44:19 +0200

    iteración 2

--- JSReviewBasics.js
+++ JSReviewBasics.js
@@ -26,3 +26,59 @@ console.log(categories);
 // **Iteración #2: Mix Fors**
 
 // Dado el siguiente javascript usa forof y forin para hacer la media del volumen de todos los sonidos favoritos que tienen los usuarios.
+
+const users = [
+    {name: 'Manolo el del bombo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 50},
+            rain: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'Mortadelo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 30},
+            shower: {format: 'ogg', volume: 55},
+            train: {format: 'mp3', volume: 60},
+        }
+    },
+    {name: 'Super Lopez',
+        favoritesSounds: {
+            shower: {format: 'mp3', volume: 50},
+            train: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'El culebra',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 67},
+            wind: {format: 'ogg', volume: 35},
+            firecamp: {format: 'mp3', volume: 60},
+        }
+    },
+]
+
+let volume = [];
+
+for (const user of users) {
+    for (const key in user.favoritesSounds) {
+        volume.push(user.favoritesSounds[key].volume);
+    };
+};
+
+console.log(volume);
+
+let i = 0;
+for (const vol of volume) {
+    i += vol;
+};
+let media = i / volume.length;
+
+console.log(media);
+
+// **Iteración #3: Mix Fors**
+
+// Dado el siguiente javascript usa forof y forin para saber cuantas veces ha sido cada sonido agregado por los usuarios a favorito. Para ello recorre la lista de usuarios y usa forin para recoger el nombre de los sonidos que el usuario tenga como favoritos.
+// Una vez accedas a ellos piensa en la mejor forma de hacer un conteo de cada vez que ese sonido se repita como favorito en cada usuario.
+
+// Este ejercicio es un poco complicado con los conocimientos actuales pero...a la vez un buen reto y oportunidad para comprender que hay muchas formas de hacer las cosas en javascript.
